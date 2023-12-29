cdae8869ea9466396f73aafffa0ae5142e86c86f
Kirill Suzev <kirill.suzev@huawei.com>
2023-12-29 13:00:25 +0300

    Small refactoring

--- 2023/10.cpp
+++ 2023/10.cpp
@@ -6,12 +6,31 @@
 #include <vector>
 #include <list>
 #include <stdio.h>
-#include <unordered_map>
+#include <unordered_set>
 
 #include "common.h"
 #include "10.h"
 
 namespace {
+
+struct hash_pair {
+    template <class T1, class T2>
+    size_t operator()(const std::pair<T1, T2>& p) const
+    {
+        auto hash1 = std::hash<T1>{}(p.first);
+        auto hash2 = std::hash<T2>{}(p.second);
+
+        if (hash1 != hash2) {
+            return hash1 ^ hash2;
+        }
+
+        // If hash1 == hash2, their XOR is zero.
+        return hash1;
+    }
+};
+
+typedef std::pair<int, int> coords;
+typedef std::unordered_set<coords, hash_pair> Coords;
 
 class Cell {
 public:
@@ -53,6 +72,10 @@ public:
         cell_ = cell;
     }
 
+    char get_cell() const {
+        return cell_;
+    }
+
 private:
     char cell_;
 };
@@ -63,7 +86,7 @@ public:
         maze_.emplace_back();
         auto& new_row = maze_.back();
 
-        for (size_t i = 0; i < line.size(); ++i) {
+        for (int i = 0; i < line.size(); ++i) {
             char ch = line[i];
             new_row.emplace_back(ch);
 
@@ -77,7 +100,70 @@ public:
 
     long get_step_count() {
         update_start_cell();
-        return 0;
+        const auto visited = traverse_loop();
+        print_maze(visited);
+
+        std::cout << "Steps count = " << visited.size() << std::endl; 
+        VERIFY(visited.size() % 2 == 0, << "Wrong amount of steps");
+        return visited.size() / 2;
+    }
+
+private:
+    Coords traverse_loop() {
+        Coords visited;
+
+        int x = start_x_;
+        int y = start_y_;
+
+        while(true) {
+            const auto coord = std::make_pair(x, y);
+            VERIFY(!visited.contains(coord), << "Already visited x = " << x << " y = " << y);
+
+            visited.insert(coord);
+            const Cell& current_cell = get_cell(x, y);
+
+            if (current_cell.can_go_north() && !visited.contains(std::make_pair(x, y - 1))) {
+                y = y - 1;
+                continue;
+            }
+
+            if (current_cell.can_go_east() && !visited.contains(std::make_pair(x + 1, y))) {
+                x = x + 1;
+                continue;
+            }
+
+            if (current_cell.can_go_south() && !visited.contains(std::make_pair(x, y + 1))) {
+                y = y + 1;
+                continue;
+            }
+
+            if (current_cell.can_go_west() && !visited.contains(std::make_pair(x - 1, y))) {
+                x = x - 1;
+                continue;
+            }
+
+            std::cout << "Loop is done" << std::endl;
+            break;
+        }
+
+        return visited;
+    }
+
+    void print_maze(const Coords& visited) {
+        std::cout << "Maze: " << std::endl;
+
+        for (int y = 0; y < maze_.size(); ++y) {
+            const auto& row = maze_[y];
+            for (int x = 0; x < row.size(); ++x) {
+                if (!visited.contains(std::make_pair(x, y))) {
+                    std::cout << '.';
+                } else {
+                    std::cout << '#';
+                }
+            }
+
+            std::cout << std::endl;
+        }
     }
 
 
@@ -85,17 +171,70 @@ private:
     void update_start_cell() {
         VERIFY(start_x_ != -1 && start_y_ != -1, << "Start pos is not initialized");
 
+        Cell north_cell = get_cell(start_x_, start_y_ - 1);
+        Cell east_cell = get_cell(start_x_ + 1, start_y_);
+        Cell south_cell = get_cell(start_x_, start_y_ + 1);
+        Cell west_cell = get_cell(start_x_ - 1, start_y_);
 
+        char new_value = GROUND;
+
+        if (north_cell.can_go_south() && south_cell.can_go_north()) {
+            VERIFY(new_value == GROUND, << "new value already updated");
+            new_value = '|';
+        }
+
+        if (west_cell.can_go_east() && east_cell.can_go_west()) {
+            VERIFY(new_value == GROUND, << "new value already updated");
+            new_value = '-';
+        }
+
+        if (north_cell.can_go_south() && east_cell.can_go_west()) {
+            VERIFY(new_value == GROUND, << "new value already updated");
+            new_value = 'L';
+        }
+
+        if (north_cell.can_go_south() && west_cell.can_go_east()) {
+            VERIFY(new_value == GROUND, << "new value already updated");
+            new_value = 'J';
+        }
+
+        if (west_cell.can_go_east() && south_cell.can_go_north()) {
+            VERIFY(new_value == GROUND, << "new value already updated");
+            new_value = '7';
+        }
+
+        if (east_cell.can_go_west() && south_cell.can_go_north()) {
+            VERIFY(new_value == GROUND, << "new value already updated");
+            new_value = 'F';
+        }
+
+        VERIFY(new_value != GROUND, << "new value has not been updated");
+        
+        std::cout << "Starting point is " << new_value << std::endl;
+        maze_[start_y_][start_x_] = new_value;
     }
 
+    Cell get_cell(int x, int y) const {
+        if (y < 0 || y >= maze_.size()) {
+            return Cell(GROUND);
+        }
+
+        const auto& row = maze_[y];
+        if (x < 0 || x >= row.size()) {
+            return Cell(GROUND);
+        }
+
+        return Cell(row[x]);
+    }
 
 private:
     std::vector<std::vector<Cell>> maze_;
-    size_t start_x_ = -1;
-    size_t start_y_ = -1;
+    int start_x_ = -1;
+    int start_y_ = -1;
 
 private:
     static const char START = 'S';
+    static const char GROUND = '.';
 };
 
 
@@ -114,7 +253,8 @@ int main() {
         solver.add_line(line);
     }
 
-    std::cout << "Result is " << solver.get_step_count() << std::endl;
+    int result = solver.get_step_count();
+    std::cout << "Result is " << result << std::endl;
     return 0;
 }
 
